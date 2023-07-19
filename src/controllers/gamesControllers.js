7d61887a7cb3bfd7d6aa74ffa61dd10232d4ce98
Gustavo Russo <russo.gustavo@gmail.com>
2023-07-19 18:44:52 -0300

    Correji las imagenes

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const games=JSON.parse( fs.readFileSync(path.join(__dirname,"../data/games.json"),"utf-8"));
+const gamesFilePath = path.join(__dirname, '../data/games.json');
+const games=JSON.parse( fs.readFileSync(gamesFilePath,"utf-8"));
 const gamesControllers={
   index:function (req,res) {
     
@@ -19,13 +20,12 @@ const gamesControllers={
     newgame={
       id:Math.random()*10000,
       title:req.body.title,
-      Imagen:req.file? "img" + req.file.filename,"https://depor.com/resizer/ryjpVYp7gvye_0LgXZYRO_yL-B0=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/MBJ5QO7YPRHIXI26D3OQM6KUZQ.JPG",
+      Imagen:req.file? "/img/" + req.file.filename:"https://depor.com/resizer/ryjpVYp7gvye_0LgXZYRO_yL-B0=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/MBJ5QO7YPRHIXI26D3OQM6KUZQ.JPG",
       Description:req.body.title + ": Descripcion",
       genero:req.body.genre
     };
     games.push(newgame);
-    const gamesJson=JSON.stringify(games,null," ")
-    fs.writeFileSync(path.join(__dirname,"../data/games.json"),"utf-8",gamesJson);
+    fs.writeFileSync(gamesFilePath, JSON.stringify(games,null,' '));
 
 
     res.render("games/create");
